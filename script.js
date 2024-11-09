adefa7c7e606e0fb3dbdf19b048681d4e9d5606c
phuhs <phuhs@coin98.finance>
2024-11-09 12:37:25 +0700

    test

--- script.js
+++ script.js
@@ -2,20 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const params = new URLSearchParams(window.location.search);
   const messageElement = document.getElementById("message");
 
-  if (params.has("p")) {
+  if (params.has("redirect_uri")) {
     // Handle error
-    const p = params.get("p");
-
+    const redirect_uri = params.get("redirect_uri");
+    window.location.replace(redirect_uri);
+  } else if (params.has("p")) {
     window.location.replace("http://localhost:3000?p=" + p);
-  } else if (params.has("code")) {
-    // Process the authorization code
-    const code = params.get("code");
-    messageElement.textContent = `Authorization code received: ${code}`;
-    messageElement.style.color = "green";
-
-    // You might want to send this code to your server for further processing
-    // fetch('/process-code', { method: 'POST', body: JSON.stringify({ code }) })
   } else {
     messageElement.textContent = "No code or error received.";
   }
 });
+//
